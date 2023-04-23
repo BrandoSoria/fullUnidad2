7df9e103f9d32b1d5f0b6ed9aa6a20a883affb99
brandon antonio soria gomez <elsoria222@gmail.com>
2023-04-22 23:54:31 -0600

    nuevo

--- ModuloHTTP win.js
+++ ModuloHTTP win.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const hostname = '192.168.1.26';
+const hostname = '127.0.0.';
 const port = 3000;
 const server = http.createServer((req, res) =>{
     res.statusCode = 200;
